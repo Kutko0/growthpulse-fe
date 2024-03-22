099778e945f28b65a0b5a25351b85482e8e7fbce
Andrej Kutliak <andrej.kutliak@sky.uk>
2024-03-22 10:26:06 +0100

    restructuring the file system

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavigationComponent } from './navigation/navigation.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ValuePropositionLandingComponent } from './components/value-proposition-landing/value-proposition-landing.component';
+import { LandingCoverComponent } from './components/landing-cover/landing-cover.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    ValuePropositionLandingComponent,
+    LandingCoverComponent
   ],
   imports: [
     BrowserModule,
---
+++ app/src/app/components/landing-cover/landing-cover.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-cover',
+  templateUrl: './landing-cover.component.html',
+  styleUrls: ['./landing-cover.component.css']
+})
+export class LandingCoverComponent {
+
+}
